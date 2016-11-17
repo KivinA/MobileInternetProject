63c819fc2d762c38e07141e4209ac5c716e4bfd2
RoelandsAntony <antony.roelands@student.odisee.be>
2016-11-17 12:02:39 +0100

    update 1
    JS registreer / function vergeten / style

--- JS/project.js
+++ JS/project.js
@@ -1,7 +1,26 @@
 "use strict";
 /*global $, console
 */
+
+function vergeten()
+{
+
+    
+   if($(".divLogin").is(":visible"))
+        {
+            $(".divLogin").hide();
+            $(".divRegister").show(1000);
+        }
+    else{
+        
+        $(".divRegister").show(1000);
+   
+    }   
+}
+
+
+
 $(document).ready(function ()
     {
-        $(".divLogin").hide();
+        $(".divRegister").hide();
     });
