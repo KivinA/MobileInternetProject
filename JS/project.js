d8542d723e327578121b5acd63c279252a940e90
RoelandsAntony <antony.roelands@student.odisee.be>
2016-11-18 00:02:16 +0100

    Update 18/11/2016 - 00.01
    Nieuwe JS (Nieuwe functies, nieuwe links aan knoppen),
    JS opschonen,
    Aangepaste CSS,
    HTML pagina aangepast

--- JS/project.js
+++ JS/project.js
@@ -2,22 +2,38 @@
 /*global $, console
 */
 
-function register()
-{
+function register() {
     switchPage(".divLogin", ".divRegister");
     console.log("Works..");
 }
 
-function switchPage(frompage, topage)
-{
+function forgot() {
+    switchPage(".divLogin", '#divForgot');
+    console.log("Vergeten zeg...");
+}
+
+function backHome() {
+    switchPage(".divRegister", ".divLogin");
+    console.log("terug");
+}
+
+function backHome2() {
+    switchPage('#divForgot', ".divLogin");
+    console.log("terug");
+}
+
+
+function switchPage(frompage, topage) {
     $(frompage).hide();
     $(topage).show(500);
 }
 
-$(document).ready(function ()
-    {
+$(document).ready(function () {
     /*
      *  BINDING ALL BUTTONS, LINKS, etc.
      */
-        $('#aForgot').on("touchstart", register);
-    });
+    $('#register').on("touchstart", register);
+    $('#backReg').on("touchstart", backHome);
+    $('#aForgot').on("touchstart", forgot);
+    $('#backForgot').on("touchstart", backHome2);
+});
